510496ce964a77e0462678d58ff0a2dedb67ede5
mcspe <mcspezzaferro@gmail.com>
2023-05-04 15:21:25 +0200

    [add] milestone 1

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,11 +1,29 @@
 <script>
+  import { store } from '../data/store.js'; 
+  import Card from './partials/Card.vue';
+
   export default {
-    name: 'Main'
+    name: 'Main',
+    components:{
+      Card,
+    },
+    data() {
+      return {
+        store
+      }
+    }
   }
 </script>
 
 <template>
-  
+  <Card 
+    v-for="media in store.searchResult.results"
+    :key="media.id" 
+    :title="media.title"
+    :originalTitle="media.original_title"
+    :language="media.original_language"
+    :vote="media.vote_average"
+    />
 </template>  
 
 <style lang="scss" scoped>
---
+++ src/components/partials/Card.vue
@@ -0,0 +1,24 @@
+<script>
+  export default {
+    name: 'Card',
+    props: {
+      title: String,
+      originalTitle: String,
+      language: String,
+      vote: Number
+    }
+  }
+</script>
+
+<template>
+  <ul>
+    <li>{{ title }}</li>
+    <li>{{ originalTitle }}</li>
+    <li>{{ language }}</li>
+    <li>{{ vote }}</li>
+  </ul>
+</template>  
+
+<style lang="scss" scoped>
+
+</style>
