9fd4faaeb0395c9462f0381f2aa302d0530d7e1d
mcspe <mcspezzaferro@gmail.com>
2023-05-05 17:23:01 +0200

    [add] loading window

--- src/data/store.js
+++ src/data/store.js
@@ -1,5 +1,11 @@
 import { reactive } from 'vue';
 export const store = reactive({
+  accounts: [
+    {
+      name: 'Marco',
+      
+    }
+  ]
   getImgUrl: 'https://api.themoviedb.org/3/configuration',
   imgInfo: {},
   apiUrl: 'https://api.themoviedb.org/3/search/',
