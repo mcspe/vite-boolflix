7d180058195b929a1926503aae84708f59b32b07
mcspe <mcspezzaferro@gmail.com>
2023-05-08 17:47:32 +0200

    [add] searchbar style

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -10,6 +10,7 @@
     methods: {
       startSearch() {
         store.apiParams.page = 1;
+        store.searchBarString = '';
         this.$emit('startSearch');
       }
     }
@@ -17,41 +18,48 @@
 </script>
 
 <template>
-  <header class="d-flex align-items-center">
-    <div class="ms-logo h-100 p-3">
-      <img src="/logo-boolflix.png" alt="Logo Boolflix">
+  <header>
+    <div class="account-bar float-end d-flex align-items-center gap-3 h-100 p-3">
+      <div class="search rounded-pill d-flex align-items-center gap-3 h-100 p-2">
+        <input 
+          type="text" 
+          placeholder="Cerca" 
+          v-model="store.searchBarString"
+          @keyup.enter="startSearch">
+        <select 
+          name="media" 
+          id="media"
+          v-model="store.selectedMedia"
+          >
+          <option value="movie">Film</option>
+          <option value="tv">Serie TV</option>
+          <option value="multi">Tutto</option>
+        </select>
+        <span @click="startSearch"><font-awesome-icon icon="fa-solid fa-magnifying-glass" /></span>
+      </div>
+      <div class="account-avatar h-100" :style="`background: ${store.activeAccount.avatar}`">
+        <img src="/avatar.png" alt="avatar" class="img-fluid">
+      </div>
     </div>
-    <nav class="h100">
-      <ul class="h100 m-0">
-        <li
-          v-for="(page, i) in store.pages"
-          :key="i"
-          class="h-100 d-inline-block mx-2">
-          <button
-            @click="$emit('goToPage', page)">
-            {{ page }}
-          </button>
-        </li>
-      </ul>
-    </nav>
-    <div class="float-right">
-      <input 
-        type="text" 
-        placeholder="Cerca" 
-        v-model="store.searchBarString"
-        @keyup.enter="startSearch">
-      <button @click="startSearch">Cerca</button>
-      <label for="media">Scegli la categoria</label>
-      <select 
-        name="media" 
-        id="media"
-        v-model="store.selectedMedia"
-        >
-        <option value="movie">Film</option>
-        <option value="tv">Serie TV</option>
-        <option value="multi">Tutto</option>
-      </select>
+    <div class="d-flex align-items-center h-100">
+      <div class="ms-logo h-100 p-3">
+        <img src="/logo-boolflix.png" alt="Logo Boolflix">
+      </div>
+      <nav class="h100">
+        <ul class="h100 m-0">
+          <li
+            v-for="(page, i) in store.pages"
+            :key="i"
+            class="h-100 d-inline-block mx-2">
+            <button
+              @click="$emit('goToPage', page)">
+              {{ page }}
+            </button>
+          </li>
+        </ul>
+      </nav>
     </div>
+    
   </header>
 </template>  
 
@@ -60,6 +68,25 @@
   header {
     height: 4.5rem;
     margin-bottom: .5rem;
+
+    .account-bar {
+      .search {
+        background: whitesmoke;
+        overflow: hidden;
+
+        input, select {
+          border: none;
+          background: none;
+        }
+        span {
+          cursor: pointer;
+        }
+      }
+      .account-avatar {
+        width: 2.5rem;
+        cursor: pointer;
+      }
+    }
 
     .ms-logo {
       img {
--- src/main.js
+++ src/main.js
@@ -8,8 +8,9 @@ import { faStar as fasStar } from '@fortawesome/free-solid-svg-icons';
 import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 import { faChevronUp } from '@fortawesome/free-solid-svg-icons';
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
-library.add(farStar, fasStar, faChevronDown, faChevronUp, faXmark);
+library.add(farStar, fasStar, faChevronDown, faChevronUp, faXmark, faMagnifyingGlass);
 
 createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
 
